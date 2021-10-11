4fd55bf746c25adf69ad3e4f6c9ada9501401f3e
schwarz-tw <schwarz.tsai@gmail.com>
2021-10-11 21:00:38 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,26 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    rows = tsv.split("\n").map { |line| line.split("\t") }
+    headers = rows.first
+    data_rows = rows[1..-1]
 
+    @data = data_rows.map do |row|
+      row.map.with_index { |cell, i| [headers[i], cell] }.to_h
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    rows = @data
+    headers = rows[0].keys
+    output = [] << headers.join("\t") << "\n"
+    rows.each { |row| output << headers.map { |header| row[header] }.join("\t") << "\n" }
+    output.join
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,17 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  require 'yaml'
+  # take_yaml: converts a String with YAML data into @data
+  # parameter: yml - a String in YAML format
+  def take_yaml(yml)
+    @data = YAML.safe_load(yml)
+  end
+
+  # to_yml: converts @data into tsv string
+  # returns: String in YAML  format
+  def to_yaml
+    @data.to_yaml
+  end
+end
